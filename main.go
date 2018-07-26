12e6885f787a8b82ea1dcb1fad102289e8a721b3
Matt Parlette <matthew.parlette@gmail.com>
2018-07-26 10:01:21 -0400

    Close todoist tasks when Jira issue is resolved

--- main.go
+++ main.go
@@ -29,13 +29,12 @@ func todoistPriority(jiraPriority string) int {
 	}
 }
 
-func createTodoistTaskfromJiraIssues(todoistClient *todoist.Client, jiraClient *jira.Client) (int, error) {
-	count := 0
-	if err := todoistClient.Sync(context.Background()); err != nil {
-		return count, err
-	}
+func getContentFromJiraIssue(issue jira.Issue) string {
+	return fmt.Sprintf("[[%v] %v - parlette.us](%v)", issue.Key, issue.Fields.Summary, fmt.Sprintf("%v/browse/%v", houseparty.Config("jira-url"), issue.Key))
+}
 
-	fmt.Println("Retrieving project ID...")
+func getTodoistWorkingProjectID(todoistClient *todoist.Client) int {
+	// fmt.Println("Retrieving project ID...")
 	// This should work to get it, but I'm getting an error that GetIDByName is undefined, so I'll do my own search
 	// project := todoistClient.Store.Projects.GetIDByName(houseparty.Config("todoist-project"))
 	project := 0
@@ -46,6 +45,32 @@ func createTodoistTaskfromJiraIssues(todoistClient *todoist.Client, jiraClient *
 		}
 	}
 
+	if project == 0 {
+		log.Fatal("Could not find project with name", houseparty.Config("todoist-project"))
+	}
+
+	return project
+}
+
+func findExistingTodoistTask(todoistClient *todoist.Client, content string) []todoist.Item {
+	var existing []todoist.Item
+	for _, item := range todoistClient.Store.Items {
+		if item.Content == content {
+			existing = append(existing, item)
+		}
+	}
+	return existing
+}
+
+func createTodoistTaskFromJiraIssues(todoistClient *todoist.Client, jiraClient *jira.Client) (int, error) {
+	fmt.Println("Creating tasks from Jira issues...")
+	count := 0
+	if err := todoistClient.Sync(context.Background()); err != nil {
+		return count, err
+	}
+
+	project := getTodoistWorkingProjectID(todoistClient)
+
 	issues, _, err := jiraClient.Issue.Search(
 		"assignee = currentUser() AND resolution = Unresolved",
 		nil)
@@ -54,16 +79,11 @@ func createTodoistTaskfromJiraIssues(todoistClient *todoist.Client, jiraClient *
 	}
 
 	for _, issue := range issues {
-		fmt.Println("Processing", issue.Key, "...")
-		content := fmt.Sprintf("[[%v] %v - parlette.us](%v)", issue.Key, issue.Fields.Summary, fmt.Sprintf("%v/browse/%v", houseparty.Config("jira-url"), issue.Key))
-		var existing []todoist.Item
-		for _, item := range todoistClient.Store.Items {
-			if item.Content == content {
-				existing = append(existing, item)
-			}
-		}
+		// fmt.Println("Processing", issue.Key, "...")
+		content := getContentFromJiraIssue(issue)
+		existing := findExistingTodoistTask(todoistClient, content)
 		if len(existing) > 0 {
-			fmt.Println("Found existing tasks, moving on...")
+			// fmt.Println("Found existing tasks, moving on...")
 			// for _, task := range existing {
 			// 	if task.IsOverDueDate() {
 			// 		fmt.Println("Task is overdue, updating due date to today...")
@@ -92,6 +112,45 @@ func createTodoistTaskfromJiraIssues(todoistClient *todoist.Client, jiraClient *
 			return count, err
 		}
 		count = count + 1
+	}
+	fmt.Println("Found", count, "Jira issues assigned to user")
+	return count, nil
+}
+
+func completeTodoistTasksFromJiraIssues(todoistClient *todoist.Client, jiraClient *jira.Client) (int, error) {
+	fmt.Println("Completing tasks from Jira issues...")
+	count := 0
+	if err := todoistClient.Sync(context.Background()); err != nil {
+		return count, err
+	}
+
+	issues, _, err := jiraClient.Issue.Search(
+		"assignee = currentUser() AND resolution != Unresolved AND updated >= startOfMonth(-1)",
+		nil)
+	if err != nil {
+		return count, err
+	}
+
+	items := []int{}
+	for _, issue := range issues {
+		content := getContentFromJiraIssue(issue)
+		existing := findExistingTodoistTask(todoistClient, content)
+		for _, task := range existing {
+			items = append(items, task.GetID())
+		}
+		count = count + 1
+	}
+	fmt.Println("Found", count, "Jira issues updated since the beginning of last month")
+	if len(items) > 0 {
+		fmt.Println("Closing", len(items), "todoist tasks...")
+		if err = todoistClient.CloseItem(context.Background(), items); err != nil {
+			return count, err
+		}
+		if err := todoistClient.Sync(context.Background()); err != nil {
+			return count, err
+		}
+	} else {
+		fmt.Println("No todoist tasks found to close, moving on...")
 	}
 	return count, nil
 }
@@ -122,14 +181,16 @@ func main() {
 	}
 
 	// First run before waiting for ticker
-	_, err = createTodoistTaskfromJiraIssues(todoistClient, jiraClient)
+	createTodoistTaskFromJiraIssues(todoistClient, jiraClient)
+	completeTodoistTasksFromJiraIssues(todoistClient, jiraClient)
 	fmt.Printf("Waiting %v seconds to run again...\n", houseparty.Config("interval"))
 
 	go func() {
 		for {
 			select {
 			case <-ticker.C:
-				createTodoistTaskfromJiraIssues(todoistClient, jiraClient)
+				createTodoistTaskFromJiraIssues(todoistClient, jiraClient)
+				completeTodoistTasksFromJiraIssues(todoistClient, jiraClient)
 				fmt.Printf("Waiting %v seconds to run again...\n", houseparty.Config("interval"))
 			case <-shutdown:
 				ticker.Stop()
--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/matthew-parlette/houseparty"
 )
 
-func TestTodoistTasksFromJira(t *testing.T) {
+func TestCreateTodoistTaskFromJiraIssues(t *testing.T) {
 	houseparty.ConfigPath = houseparty.GetEnv("CONFIG_PATH", "config")
 	houseparty.SecretsPath = houseparty.GetEnv("SECRETS_PATH", "secrets")
 	todoistClient, err := houseparty.GetTodoistClient()
@@ -18,7 +18,25 @@ func TestTodoistTasksFromJira(t *testing.T) {
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
-	count, err := createTodoistTaskfromJiraIssues(todoistClient, jiraClient)
+	count, err := createTodoistTaskFromJiraIssues(todoistClient, jiraClient)
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	fmt.Println("Processed", count, "Jira issues")
+}
+
+func TestCompleteTodoistTasksFromJiraIssues(t *testing.T) {
+	houseparty.ConfigPath = houseparty.GetEnv("CONFIG_PATH", "config")
+	houseparty.SecretsPath = houseparty.GetEnv("SECRETS_PATH", "secrets")
+	todoistClient, err := houseparty.GetTodoistClient()
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	jiraClient, err := houseparty.GetJiraClient()
+	if err != nil {
+		t.Errorf("Error: %v", err)
+	}
+	count, err := completeTodoistTasksFromJiraIssues(todoistClient, jiraClient)
 	if err != nil {
 		t.Errorf("Error: %v", err)
 	}
